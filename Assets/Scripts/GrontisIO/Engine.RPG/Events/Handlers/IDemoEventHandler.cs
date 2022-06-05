c1a4981845388eda2ad3c6a6ae0e0835fbf6c741
grontis <grontisio314@gmail.com>
2022-06-05 11:46:50 -0600

    Create events namespace

---
+++ Assets/Scripts/GrontisIO/Engine.RPG/Events/Handlers/IDemoEventHandler.cs
@@ -0,0 +1,9 @@
+using UnityEngine.EventSystems;
+
+namespace GrontisIO.Engine.RPG.Events.Handlers
+{
+    public interface IDemoEventHandler : IEventSystemHandler
+    {
+        //void OnEvent(EventData eventData);
+    }
+}
